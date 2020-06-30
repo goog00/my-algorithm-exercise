e3ba9af87b8cd7be1a0cb0e2c5d2d3a57bf14dae
sunteng <steng2009@163.com>
2020-07-01 00:51:58 +0800

    回文数

---
+++ src/main/java/Test2.java
@@ -0,0 +1,17 @@
+/**
+ * @author steng
+ * @description
+ * @date 2020-07-01 12:23 上午
+ **/
+public class Test2 {
+
+    public static void main(String[] args) {
+        int x = 12;
+        int k = x/10;
+        int t = x % 10;
+        System.out.println(x);
+        System.out.println(k);
+        System.out.println(t);
+
+    }
+}
---
+++ src/main/java/com/demo/leetcode/Solution9.java
@@ -0,0 +1,32 @@
+package com.demo.leetcode;
+
+/**
+ * 9 回文数
+ * @author steng
+ * @description
+ * @date 2020-07-01 12:27 上午
+ **/
+public class Solution9 {
+    public boolean isPalindrome(int x) {
+
+        //小于0 情况不满足
+        // 最后一位是0的情况,也不满足,因为最高位不能为0
+        //0满足要求
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertNum = 0;
+        while (x > revertNum) {
+            //x%10  对x除10取余获取x的最后一位数字
+            //x/10  对x去除最后一位数字
+
+            revertNum = revertNum * 10 + x % 10;
+            x /= 10;
+        }
+        //当x的长度为奇数时,revertNum/10去除中位数
+        return x == revertNum || x == revertNum / 10;
+
+
+    }
+}
