fdf389bbfde124bacfcaf7516c22f29393f11c41
sunteng <steng2009@163.com>
2020-07-01 23:57:41 +0800

    动态规划之按摩师,打家劫舍

--- src/main/java/com/demo/leetcode/SolutionOffer42.java
+++ src/main/java/com/demo/leetcode/SolutionOffer42.java
@@ -25,7 +25,7 @@ public class SolutionOffer42 {
     }
 
     public static void main(String[] args) {
-        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = new int[]{-2,-1};
         int k = maxSubArray(nums);
         System.out.println(k);
     }
