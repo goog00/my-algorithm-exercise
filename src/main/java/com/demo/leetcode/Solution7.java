fe2b161d8290729279bb9137d09883079a83963f
sunteng <steng2009@163.com>
2020-07-01 17:30:30 +0800

    反转整数

---
+++ src/main/java/com/demo/leetcode/Solution7.java
@@ -0,0 +1,36 @@
+package com.demo.leetcode;
+
+/**
+ * @author steng
+ * @description
+ * @date 2020-07-01 5:07 下午
+ **/
+public class Solution7 {
+
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            //从低位到高位循环获取x每一位的值
+            int pop = x % 10;
+            //x除10,去掉最后一位数
+            x /= 10;
+            if(rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && pop > 7)){
+                return 0;
+            }
+            if(rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && pop < -8)){
+                return 0;
+            }
+            //组装成反转的数组
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+}
+
+
