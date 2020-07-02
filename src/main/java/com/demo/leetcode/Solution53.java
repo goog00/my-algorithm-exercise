3179e013729f7202f8e78737b45fd4e1fe0141cf
sunteng <steng2009@163.com>
2020-07-02 12:42:29 +0800

    dp-最大子和问题

---
+++ src/main/java/com/demo/leetcode/Solution53.java
@@ -0,0 +1,22 @@
+package com.demo.leetcode;
+
+class Solution53 {
+    public int maxSubArray(int[] nums) {
+      
+        
+        int len = nums.length;
+        if(nums == null || len == 0){
+            return 0;
+        }
+        int max = nums[0];
+        int[] dp = new int[len];
+        dp[0] = nums[0] ;
+        for(int i = 1 ; i < len; i++){
+            dp[i] = Math.max(dp[i-1] + nums[i],nums[i]);
+            max = Math.max(max,dp[i]);
+        }
+        return max;
+
+
+    }
+}
